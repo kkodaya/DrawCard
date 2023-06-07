1f4ab1eba95e24c54f6aed3b4198f78180653e45
aufe1995 <120147742+aufe1995@users.noreply.github.com>
2023-06-07 10:04:44 +0900

    modify page css

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -22,9 +22,9 @@ const Header = () => {
 
                 {/* 검색 영역 */}
                 <div className='search_area'>
-                    <div class="input-container">
-                        <input type="text" name="text" class="input" placeholder="search..."/>
-                        <span class="icon"> 
+                    <div className="input-container">
+                        <input type="text" name="text" className="input" placeholder="search..."/>
+                        <span className="icon"> 
                             <svg width="19px" height="19px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="1" d="M14 5H20" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="1" d="M14 8H17" stroke="#000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M21 11.5C21 16.75 16.75 21 11.5 21C6.25 21 2 16.75 2 11.5C2 6.25 6.25 2 11.5 2" stroke="#000" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="1" d="M22 22L20 20" stroke="#000" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                         </span>
                     </div>
@@ -32,20 +32,20 @@ const Header = () => {
 
                 {/* 메뉴 영역 */}
                 <div className='menu_area'>
-                    <a class="fancy" href="#">
-                        <span class="top-key"></span>
-                        <span class="text">wallet</span>
-                        <span class="bottom-key-1"></span>
-                        <span class="bottom-key-2"></span>
+                    <a className="fancy" href="#">
+                        <span className="top-key"></span>
+                        <span className="text">wallet</span>
+                        <span className="bottom-key-1"></span>
+                        <span className="bottom-key-2"></span>
                     </a>
 
                     
                     
-                    <Link to='/mainprofile' class="fancy" href="#">
-                        <span class="top-key"></span>
-                        <span class="text">profile</span>
-                        <span class="bottom-key-1"></span>
-                        <span class="bottom-key-2"></span>
+                    <Link to='/mainprofile' className="fancy" href="#">
+                        <span className="top-key"></span>
+                        <span className="text">profile</span>
+                        <span className="bottom-key-1"></span>
+                        <span className="bottom-key-2"></span>
                     </Link>
                 </div>
             </div>
