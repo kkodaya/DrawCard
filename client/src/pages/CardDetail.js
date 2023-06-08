bffb476017da6caa44bd97590e4b2c511fca0ffb
kim yong woo <110222399+kkodaya@users.noreply.github.com>
2023-06-08 13:29:21 +0900

    feat

--- client/src/pages/CardDetail.js
+++ client/src/pages/CardDetail.js
@@ -10,11 +10,13 @@ function CardDetail() {
   // const [명함이미지 , 명함이미지변경] =useState(time)
 
   // 밑에두개는  나중에 데이터를 받아와서 넣기
-  const [designCategori,setCategori] = useState('디자인 카테고리')
-  const [designName, setDesignname] = useState('디자인이름')
-  const [designerName, setDesignername] = useState('디자이너이름')
-  const [designEx, setDesignex] = useState('디자인설명')
-  const [designPrice , setDesignPrice] = useState('디자인 가격')
+  const [designCategori,setCategori] = useState('Anna Lucia x Gees Bend Quilters: Generations')
+  const [designName, setDesignName] = useState('Generation 253')
+  const [designerName, setDesignerName] = useState('Owned by 49E635')
+  const [designEx, setDesignEx] = useState('By ARSNL_Anna_Lucia_Gees...Generations is a collaboration between generative artist Anna Lucia and the quilters of Gees Bend. Anna Lucia developed an algorithm to craft a series of digital quilts inspired by the iconic patchworks of the Quilters of Gees Bend.')
+  const [designPrice , setDesignPrice] = useState ('0.3 ETH')
+  const [designPriceWon , setDesignPriceWon] = useState ("$549.49")
+
  
 
   return (
@@ -35,9 +37,9 @@ function CardDetail() {
                       <li>{designCategori}</li>
                       <li>{designName} {designName}</li>
                       <li>{designerName}</li>
-                      <li>{designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx} {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx} {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx} {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx} {designEx}{designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx}  {designEx} {designEx} {designEx}</li>
+                      <li>{designEx}</li>
                       <li>
-                        {designPrice}
+                        {designPrice}  <span>{designPriceWon}</span>
                             <Link to="/cardbuy" className='button'>
                                 구매하기
                             </Link>
