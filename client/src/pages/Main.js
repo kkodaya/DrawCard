477cb7bb6f5111d7a561cc9508fc5750071f1a97
aufe1995 <120147742+aufe1995@users.noreply.github.com>
2023-06-08 17:42:55 +0900

    메인 페이지 수정

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,6 +2,7 @@ import './Main.css'
 import Example from '../component/img/example.png'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // 메인 페이지 작성자 이금철
 const Main = () => {
@@ -30,11 +31,27 @@ const Main = () => {
             
             {category === "ALL" && <>
             {/* 작가 프로필 홍보 영역 */}
-            <div className='main_profile'>
-                <Link to="/mainprofile" className='main_content'>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                >
+                <SwiperSlide>
+                <div className='main_profile'>
+                    <Link to="/mainprofile" className='main_content'>
+                    <div class="main_profile_title">Title</div>
                     <div className='main_profileImg'></div>
-                </Link>
-            </div>
+                    <div class="main_profile_title_arrow">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="20" width="20">
+                            <path fill="#fff" d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z"></path>
+                        </svg>
+                    </div>
+                    </Link>
+                </div>
+                </SwiperSlide>
+            </Swiper>
+
 
 
             <div className='subtitle'>최근 등록된 디자인</div>
