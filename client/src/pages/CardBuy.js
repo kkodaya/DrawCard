89a81dcd820c6c688e9a352664465153971a8f7f
aufe1995 <120147742+aufe1995@users.noreply.github.com>
2023-06-08 16:19:09 +0900

    명함 상세 페이지 수정

--- client/src/pages/CardBuy.js
+++ client/src/pages/CardBuy.js
@@ -1,6 +1,7 @@
 import React ,{ useState } from 'react';
 import Img from '../component/img/Logo.png';
 import './CardBuy.css';
+import { Link } from 'react-router-dom';
 
 // 프로필 구매 페이지 작성자 김용우
 function CardBuy() {
@@ -18,6 +19,22 @@ function CardBuy() {
       setIsModalOpen(false);
     };
 
+    const [userName , setUserName] = useState("");
+    const [userPosition , setPosition] = useState("");
+    const [userAttach , setAttach] = useState("");
+
+    const handler = e =>
+    {
+        setUserName(e.target.value);
+    }
+    const handler2 = e =>
+    {
+        setPosition(e.target.value);
+    }
+    const handler3 = e =>
+    {
+        setAttach(e.target.value);
+    }
     return(
         <div className="buy">
              {/* <header className='header'>
@@ -29,20 +46,25 @@ function CardBuy() {
                 
                 <div className='main_con'>
 
-                    <img className='left_img' src={Img} />
+                    <div className='left_img'>
+                      <img src={Img} />
+                      <span>{userName}</span>
+                      <span>{userPosition}</span>
+                      <span>{userAttach}</span>
+                    </div>
 
                     <ul className='right_text'>
                         <li>
                             <label>닉네임 :</label>
-                            <input type="text" className='nick'></input>
+                            <input type="text" className='nick' value={userName} onChange={handler}></input>
                         </li>
                         <li>
                             <label>직급 :</label>
-                            <input type="text" className='position'></input>
+                            <input type="text" className='position' value={userPosition} onChange={handler2}></input>
                         </li>
                         <li>
                            <label>소속 :</label>
-                           <input type="text" className=' attach'></input> 
+                           <input type="text" className=' attach' value={userAttach} onChange={handler3}></input> 
                         </li>                      
                     </ul>
                 </div>
@@ -50,14 +72,14 @@ function CardBuy() {
                 
                 <div className="buy_cancle">
                   <button className='buy_btn' onClick={openModal}>구매</button>
-                  <button className='cancle_btn'>취소</button>
+                  <Link to="/CardDetail" className='cancle_btn'>취소</Link>
               </div>
               
               {isModalOpen && (
                 
                 <div className="modal2">
                   
-                    <h3>입력한 내용은 바꿀수 없습니다</h3>
+                    <h3>입력한 내용은 바꿀수 없습니다 구매하시겠습니까?</h3>
                     <div className='modal_buy_cancle'>
                           <button className='modal_buy'>구매</button>
                           <button className='modal_cancle' onClick={closeModal}>취소</button>
